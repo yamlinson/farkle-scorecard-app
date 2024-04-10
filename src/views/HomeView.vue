af42ee89924b31997b73626d5df48044bc4be8d2
Ryan Tomlinson <rtomlinson118@gmail.com>
2024-04-09 21:53:20 -0600

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="main">
+    <div class="card">
+      <h2>Enter Room Code to Join a Game</h2>
+    </div>
+    <div class="card">
+      <h2>Sign In to Manage Games</h2>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
